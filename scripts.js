c6da032ef1b09649b353848f9bcb6005ee4ac6f8
AlyonaUkr <mochulska1994@gmail.com>
2023-01-11 15:16:54 +0200

    add function doMath

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
+
 function average(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -8,3 +10,29 @@ function average(arr) {
 const firstArrey = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
 const numberArrey = firstArrey.filter(item => typeof item === 'number');
 console.log(average(numberArrey));
+
+// Вивести результат математичної дії, вказаної в змінній znak. 
+// Обидва числа і знак виходять від користувача.
+
+function doMath(x, znak, y) {
+  switch (znak) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '*':
+      return x * y;
+    case '/':
+      return x / y;
+    case '%':
+      return x % y;
+    case '**':
+      return x ** y;
+    default:
+      alert('Sorry this operation do not exist');
+  } 
+}
+const x = Number(prompt('Введіть x'));
+const y = Number(prompt('Введіть y'));
+const znak = prompt('Введіть знак (+, -, *, /, %, **)');
+alert(doMath(x, znak, y));
